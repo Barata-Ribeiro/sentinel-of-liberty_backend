5ce48af244dc8efd7561438f593c99605d027c9b
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-12-05 14:47:48 -0300

    feat: implement 'UserArticleSummary' view entity

---
+++ src/entity/ArticleSummary.ts
@@ -0,0 +1,44 @@
+import { DataSource, ViewColumn, ViewEntity } from "typeorm";
+import { Article } from "./Article";
+import { Comment } from "./Comment";
+import { User } from "./User";
+
+@ViewEntity({
+    expression: (dataSource: DataSource) =>
+        dataSource
+            .createQueryBuilder()
+            .select("user.id", "userId")
+            .addSelect("user.discordUsername", "username")
+            .addSelect("article.id", "articleId")
+            .addSelect("article.title", "articleTitle")
+            .addSelect("article.contentSummary", "contentSummary")
+            .addSelect("article.createdAt", "articleCreatedAt")
+            .addSelect("COUNT(comment.id)", "commentCount")
+            .from(User, "user")
+            .leftJoin(Article, "article", "article.user = user.id")
+            .leftJoin(Comment, "comment", "comment.article = article.id")
+            .groupBy("article.id")
+            .addGroupBy("user.id")
+})
+export class UserArticleSummary {
+    @ViewColumn()
+    userId!: string;
+
+    @ViewColumn()
+    username!: string;
+
+    @ViewColumn()
+    articleId!: string;
+
+    @ViewColumn()
+    articleTitle!: string;
+
+    @ViewColumn()
+    contentSummary!: string;
+
+    @ViewColumn()
+    articleCreatedAt!: Date;
+
+    @ViewColumn()
+    commentCount!: number;
+}
