31168cac0fbb71df6a7c1d054e325f2650926e4d
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-12-07 06:41:12 -0300

    feat: implement query builder to paginate suggestions

--- src/controller/NewsSuggestionController.ts
+++ src/controller/NewsSuggestionController.ts
@@ -28,7 +28,51 @@ export class NewsSuggestionController {
         return res.status(201).json(response);
     }
 
-    async getAllNewsSuggestions(req: Request, res: Response) {}
+    async getAllNewsSuggestions(req: Request, res: Response) {
+        let { perPage, page } = req.query;
+        let realPage: number;
+        let realTake: number;
+
+        if (perPage) realTake = +perPage;
+        else {
+            perPage = "10";
+            realTake = 10;
+        }
+
+        if (page) realPage = +page === 1 ? 0 : (+page - 1) * realTake;
+        else {
+            realPage = 0;
+            page = "1";
+        }
+
+        const queryBuilder = newsSuggestionRepository
+            .createQueryBuilder("newsSuggestion")
+            .leftJoinAndSelect("newsSuggestion.user", "user")
+            .take(realTake)
+            .skip(realPage);
+
+        const [result, total] = await queryBuilder.getManyAndCount();
+        const hasNextPage = realPage + realTake < total;
+        const backendOrigin =
+            process.env.BACKEND_ORIGIN || "http://localhost:3000";
+
+        return res.status(200).json({
+            data: result,
+            perPage: realTake,
+            page: +page || 1,
+            next: hasNextPage
+                ? `${backendOrigin}/api/v1/suggestions?perPage=${realTake}&page=${
+                      +page + 1
+                  }`
+                : null,
+            prev:
+                realPage !== 0
+                    ? `${backendOrigin}/api/v1/suggestions?perPage=${realTake}&page=${
+                          +page - 1
+                      }`
+                    : null
+        });
+    }
 
     async getNewsSuggestionById(req: Request, res: Response) {
         const newsId = req.params.newsId;
