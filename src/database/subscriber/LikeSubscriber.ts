0d6cd650d12dc92e471330b8b5bbe76e0374b5af
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-01-12 06:55:44 -0300

    feat: add error handling and entity loading in LikeSubscriber

--- src/database/subscriber/LikeSubscriber.ts
+++ src/database/subscriber/LikeSubscriber.ts
@@ -6,6 +6,10 @@ import {
 } from "typeorm";
 import { Comment } from "../../entity/Comment";
 import { Like } from "../../entity/Like";
+import {
+    InternalServerError,
+    NotFoundError
+} from "../../middleware/helper/ApiError";
 
 @EventSubscriber()
 export class LikeSubscriber implements EntitySubscriberInterface<Like> {
@@ -23,13 +27,27 @@ export class LikeSubscriber implements EntitySubscriberInterface<Like> {
     }
 
     async beforeRemove(event: RemoveEvent<Like>) {
-        if (event.entityId) {
-            const commentRepository = event.manager.getRepository(Comment);
-            await commentRepository.decrement(
-                { id: event.entity?.comment.id },
-                "likeCount",
-                1
+        const commentRepository = event.manager.getRepository(Comment);
+        let commentId: string;
+
+        if (event.entity?.comment) commentId = event.entity.comment.id;
+        else if (event.entityId) {
+            // Load the Like entity if it's not available
+            const like = await event.manager.findOne(Like, {
+                where: { id: event.entityId },
+                relations: ["comment"]
+            });
+            if (!like || !like.comment)
+                throw new NotFoundError(
+                    "Like entity or associated comment not found"
+                );
+
+            commentId = like.comment.id;
+        } else
+            throw new InternalServerError(
+                "Neither entity nor entityId available"
             );
-        }
+
+        await commentRepository.decrement({ id: commentId }, "likeCount", 1);
     }
 }
