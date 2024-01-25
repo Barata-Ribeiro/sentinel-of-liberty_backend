c56432b137d74f55f493d636bcbe957169f64120
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-01-25 06:42:44 -0300

    docs: add proper documentation to HomeController

--- src/controller/HomeController.ts
+++ src/controller/HomeController.ts
@@ -4,6 +4,14 @@ import { userArticleSummaryRepository } from "../repository/articleSummariesRepo
 import { newsSuggestionRepository } from "../repository/newsSuggestionRepository";
 
 export class HomeController {
+    /**
+     * Retrieves the home-page content. It returns the first 10 articles and the first 10 news suggestions. The articles are ordered by creation date, descending.
+     * If not data exists, it returns an empty array and the client should handle it.
+     *
+     * @param _req - The request object.
+     * @param res - The response object.
+     * @returns A JSON containing two arrays: The latest 10 articles and the latests 10 news suggestions.
+     */
     async getHomeContent(_req: Request, res: Response): Promise<Response> {
         const firstTenArticles = await userArticleSummaryRepository.find({
             take: 10,
